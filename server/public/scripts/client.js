ed0e91f995dfa237fd6e7109ef9710b61bafcdb4
Emi Chen <emimbchen@gmail.com>
2017-10-12 17:05:03 -0500

    completed inital post and made random number

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,4 +3,28 @@ console.log("i'm here")
 $(document).ready(inQuery);
 function inQuery(){
     console.log('query in');
+    $(".difficulty").on('click','button',choseDifficulty);
+    $("#startGame").on('click', startGame);
+
+}
+
+var max; 
+
+function choseDifficulty(){
+    max = $(this).data('difficulty');
+    console.log(max);
+    return max;
+}
+
+function startGame(){
+    $.ajax({
+        method: "POST",
+        url: "/startgame",
+        data: {
+            difficulty: max
+        }
+    })
+    .done(function(response){
+        response
+    })
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var randomnumber = require('./module/randomnumber.js');
 var app = express();
 var port = 5000;
 
 var bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('server/public'));
@@ -10,3 +12,12 @@ app.use(express.static('server/public'));
 app.listen(port, function(){
     console.log('listening on port ', port);
 });
+
+var max;
+var currentNumber;
+
+app.post('/startgame',function(req, res){
+    max = parseInt((req.body).difficulty);
+    currentNumber = randomnumber(0, max);
+    console.log(currentNumber);
+})
